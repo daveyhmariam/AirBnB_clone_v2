f9ed33099208793ae49f04fb44253eca311955dc
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 01:17:31 +0300

    ORM model of database

--- models/base_model.py
+++ models/base_model.py
@@ -11,34 +11,35 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True)
+    id = Column(String(60),unique=True, nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            from os import getenv
-            if getenv('HBNB_TYPE_STORAGE') != 'db':
-                for k in ['updated_at', 'created_at']:
-                    if k in kwargs.keys():
-                        kwargs[k] = datetime.fromisoformat(kwargs[k])
-                if '__class__' in kwargs.keys():
-                    del kwargs['__class__']
-
-            if 'created_at' not in kwargs.keys():
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
                 self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-
-            for key,  value in kwargs.items():
-                setattr(self, key, value)
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
