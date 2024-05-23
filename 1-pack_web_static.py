229d612fd6e1b203e5c94fe6d4ec94cb2f738b09
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-23 13:52:09 +0300

    Fabric script that generates a .tgz archive from the contents of the web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" archive a directory to tgz
+"""
+import os
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    """fabric script to create archive
+    """
+    dtime = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dtime.year,
+                                                         dtime.month,
+                                                         dtime.day,
+                                                         dtime.hour,
+                                                         dtime.minute,
+                                                         dtime.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
