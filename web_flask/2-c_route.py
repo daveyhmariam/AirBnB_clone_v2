2be85ee4dc1e6e803c365349b6ad748240c969f0
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-23 20:43:14 +0300

    dynamic url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""simple flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Display HBHB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<tex>", strict_slashes=False)
+def cfun(tex):
+    return ("C {}".format(tex.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
