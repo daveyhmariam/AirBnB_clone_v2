c8861cdc2106fdba3ab28e91731e8f8b75c3e7b8
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-23 21:29:03 +0300

    dynamic url with datatype

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""simple flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Display HBHB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<tex>", strict_slashes=False)
+def cfun(tex):
+    return ("C {}".format(tex.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
