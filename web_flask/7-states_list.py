f5257eeb13b206dcc1c9dead46a469f2cbc014db
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-25 00:22:02 +0300

    module documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
+"""
+A simple Flask web application that displays a list of all State objects 
+stored in the database.
+
+Routes:
+    /states_list: Displays an HTML page with a list of all State objects.
+    
+Functions:
+    states_list: Fetches and renders the list of states.
+    closedown: Closes the SQLAlchemy session.
+"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
 
-    states are sorted in ascending
+    The states are sorted in ascending order by name.
+
+    Returns:
+        The rendered HTML template displaying the list of states.
     """
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
-
 @app.teardown_appcontext
 def closedown(exec):
-    """close the SQLAlchemy session."""
-    storage.close()
+    """
+    Closes the SQLAlchemy session after each request.
 
+    Args:
+        exec: The exception that was raised (if any).
+    """
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
