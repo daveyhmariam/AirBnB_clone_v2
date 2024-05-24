6fbba6f336de698e79a0a7876e9a738af9e663ff
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-25 00:14:23 +0300

    database connection with flask and sqlalchemy rendering template to represent data

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""simple flask application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def closedown(exec):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
