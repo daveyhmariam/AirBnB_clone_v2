45dd9021720874bbdbe42766465faabf7ea81686
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-25 00:18:06 +0300

    module documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,12 +11,16 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    """
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def closedown(exec):
+    """close the SQLAlchemy session."""
     storage.close()
 
 
